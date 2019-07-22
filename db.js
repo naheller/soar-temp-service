31102c8b3a4718aec7d4a2633fb5be31245a9c5a
Nathan Heller <naheller@gmail.com>
2019-07-21 19:31:17 -0700

    use mlab mongoDB

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/soar'
+    // DB: 'mongodb://localhost:27017/soar'
+    DB: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds119129.mlab.com:19129/soar`
 }
